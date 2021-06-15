d4043480dc5cf529d93356d4a46854fd35029ac7
Jeffrey Wong <jwong@dayspring-tech.com>
2021-06-15 07:57:08 -0700

    php_ini recipe should notify httpd to restart

--- vagrant_webapp/recipes/php_ini.rb
+++ vagrant_webapp/recipes/php_ini.rb
@@ -1,7 +1,13 @@
+service "httpd" do
+  action :nothing
+end
+
 template "/etc/php.d/zzz.php-override.ini" do
   source "php-override.ini.erb"
   mode 0644
   if params[:cookbook]
     cookbook params[:cookbook]
   end
+
+  notifies :restart, "service[httpd]", :delayed
 end
