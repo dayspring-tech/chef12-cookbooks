dbd8c24120aae55af0ddbb5d3c7617d389b9989d
Jeffrey Wong <jwong@dayspring-tech.com>
2022-05-11 11:19:05 -0700

    add debugging

--- hosts/recipes/default.rb
+++ hosts/recipes/default.rb
@@ -1,5 +1,8 @@
 instance = search("aws_opsworks_instance", "self:true").first
 
+Chef::Log.info(search("aws_opsworks_instance"))
+Chef::Log.info(generate_hosts_entries)
+
 template "/etc/hosts" do
   source "hosts.erb"
   mode "0644"
