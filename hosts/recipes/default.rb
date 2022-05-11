b3d610208e0fa387908593e479712097fe697893
Jeffrey Wong <jwong@dayspring-tech.com>
2022-05-11 11:10:52 -0700

    trial run for overriding hosts file generation

---
+++ hosts/recipes/default.rb
@@ -0,0 +1,10 @@
+instance = search("aws_opsworks_instance", "self:true").first
+
+template "/etc/hosts" do
+  source "hosts.erb"
+  mode "0644"
+  variables(
+    :localhost_name => instance["hostname"],
+    :nodes => generate_hosts_entries
+  )
+end
